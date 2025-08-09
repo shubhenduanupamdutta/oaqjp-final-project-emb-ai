923be49dc484af55d67516fafcab71c27efc7e08
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-08-09 11:14:39 +0530

    Better style - resolve ruff warnings

--- EmotionDetection/__init__.py
+++ EmotionDetection/__init__.py
@@ -1,6 +1,8 @@
 """Emotion detection package.
 
 This package provides functionality for detecting emotions from text.
-"""
+"""  # noqa: N999
 
 from . import emotion_detection
+
+__all__ = ["emotion_detection"]
