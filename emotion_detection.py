2fbdc2123e4f40dca15efe8a5eb06146d9a23ca2
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-08-09 11:03:41 +0530

    Returning a dictionary

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,9 +1,11 @@
 """Module for emotion detection using WATSON NLP. Assume WATSON is pre-configured."""
 
+import json
+
 import requests
 
 
-def emotion_detector(text_to_analyze: str) -> str | None:
+def emotion_detector(text_to_analyze: str) -> dict[str, float | str] | dict[str, None]:
     """Detect emotions in a given text using WATSON NLP Emotion Predict function.
 
     Args:
@@ -17,6 +19,25 @@ def emotion_detector(text_to_analyze: str) -> str | None:
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = {"raw_document": {"text": text_to_analyze}}
     response = requests.post(url, headers=headers, json=input_json, timeout=60)
+    default_emotions = {
+        "anger": None,
+        "disgust": None,
+        "fear": None,
+        "joy": None,
+        "sadness": None,
+        "dominant_emotion": None,
+    }
+
     if response.status_code != requests.codes.ok:
-        return None
-    return response.text
+        return default_emotions
+
+    predictions = json.loads(response.text)["emotionPredictions"]
+    emotions: dict[str, float | str] = predictions[0].get("emotion", {})
+
+    if not emotions:
+        return default_emotions
+
+    dominant_emotion: str = max(emotions, key=emotions.get)  # type: ignore  # noqa: PGH003
+
+    emotions.update({"dominant_emotion": dominant_emotion})
+    return emotions
